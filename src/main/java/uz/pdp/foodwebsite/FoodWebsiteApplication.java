8ef83344368f6dba73004216041479c561b5d82b
m1lymoe17 <muydinovumidjon2005@gmail.com>
2022-11-29 11:41:48 +0500

    first commit

---
+++ src/main/java/uz/pdp/foodwebsite/FoodWebsiteApplication.java
@@ -0,0 +1,13 @@
+package uz.pdp.foodwebsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodWebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodWebsiteApplication.class, args);
+	}
+
+}
